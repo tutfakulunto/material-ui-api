08bcaed8e70dfc2cd0f1c0f420736efa51144526
Scott Johnson <c.scott.johnson@gmail.com>
2018-01-16 19:23:43 -0600

    added logging and a server.log

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const fs = require('fs');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 /* GET route for /languages */
 router.get('/', (req, res) => {
     const languages = require('../fixtures/languages.json');
-    res.status(200).json({ languages });
+    res.status(200).json({languages});
 });
 app.use('/languages', router);
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
 
 app.listen(3000, () => console.log('Example app listening on port!'));
