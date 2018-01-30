e2497963d3a7565efae893f2eafcfc89952fd47d
Scott Johnson <c.scott.johnson@gmail.com>
2018-01-29 19:52:28 -0600

    most recent changes

---
+++ src/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '971484670446-9qki2iaofkummc5797rgqro918loukil.apps.googleusercontent.com',
+    googleClientSecret: 'iNtzYRrIGuYk2YEqTa0sQKi1',
+    mongoURI: 'mongodb://tutfakulunto:Eiffel80@ds117148.mlab.com:17148/linguacious',
+    cookieKey: 'ajdirqeopvncmajrfiqeor'
+}
